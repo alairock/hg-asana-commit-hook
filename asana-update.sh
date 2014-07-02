a390059f0ddca540346ac67e1c250f5822807295
Chuck Burgess <cdburgess@gmail.com>
2014-07-02 11:28:01 -0600

    Adding the ability to tag the asana ticket(s) for upload. When you add #upload to the commit message, it will tag every #{TICKET} with the upload tag based on the upload-tag value you have added to the .hgrc

--- asana-update.sh
+++ asana-update.sh
@@ -4,12 +4,17 @@
 # necessary .hgrc configuration:
 # [asana]
 # asana-key = {ASANAKEY}
+# upload-tag = {TAGID}
 # -----------------------
 apikey=$(hg showconfig asana.asana-key)
+upload_tag=$(hg showconfig asana.upload-tag)
+
 if [ $apikey == '' ] ; then exit 0; fi
 
 # regex pattern to recognize a story number
 taskid_pattern='#([0-9]*)'
+
+upload_pattern='#upload'
 
 # get the checkin comment for parsing
 summary=$(hg log -vr $HG_NODE | egrep -v '^(tag|parent|date|user|files):' | sed 's/^description:$//')
@@ -19,7 +24,7 @@ summary=${summary//&/%26}
 files=$(hg status --change $HG_NODE)
 
 comment="$summary
------
+------------------
 $files"
 
 # break the commit comment down into words
@@ -31,6 +36,9 @@ do
     if [[ $element =~ $taskid_pattern ]]; then
     referenced=("${referenced[@]}" "${BASH_REMATCH[1]}")
     fi
+    if [[ $element =~ $upload_pattern ]]; then
+    uploaded=1
+    fi
 done
 
 # touch the stories we've referenced
@@ -38,4 +46,8 @@ for element in "${referenced[@]}"
 do
     curl -u ${apikey}: https://app.asana.com/api/1.0/tasks/${element}/stories \
          -d "text=Mercurial commit: ${comment/ /:%0A}" > /dev/null 2>&1
+    if [[ $uploaded == 1 && $upload_tag != '' ]]; then
+    	curl -u ${apikey}: https://app.asana.com/api/1.0/tasks/${element}/addTag \
+         -d "tag=${upload_tag}" > /dev/null 2>&1
+    fi
 done
